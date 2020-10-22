0b1103dc69335eae19da24cb95d43e4fc27d06ef
zven21 <andzven@gmail.com>
2020-10-22 14:32:52 +0800

    go

---
+++ README.md
@@ -0,0 +1,6 @@
+
+```shell
+
+PORT=4000 iex --name webapp@127.0.0.1 -S mix phx.server
+PORT=4001 iex --name opapp@127.0.0.1 -S mix phx.server
+```
--- webapp/config/dev.exs
+++ webapp/config/dev.exs
@@ -16,7 +16,7 @@ config :webapp, Webapp.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :webapp, WebappWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT")],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
